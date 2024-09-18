b9bc0c77a8b566ec959422f88526642bbbe4377a
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2024-09-18 14:19:07 +0300

    Some commit

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -4,30 +4,37 @@
     {        
         Console.WriteLine("Введите предложение:");
         string input = Console.ReadLine();
-
-        // Разделение предложения на слова по пробелам
+        
         string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
         
         int wordCount = words.Length;
         Console.WriteLine($"Количество слов в предложении: {wordCount}");
-
-        // первая буква каждого слова становится заглавной
+        
         string[] capitalizedWords = words.Select(word => ToUpper(word)).ToArray();
-
-        // Сортировка слов в алфавитном порядке
+        
         Array.Sort(capitalizedWords);
         
         Console.WriteLine("Слова в отсортированном виде:");
+
         foreach (string word in capitalizedWords)
         {
             Console.WriteLine(word);
         }
     }
 
-    // Метод для преобразования первой буквы слова в заглавную
+    /// <summary>
+    /// Converts the first letter of the given word to uppercase, and the rest to lowercase.
+    /// </summary>
+    /// <param name="word">The word to be transformed.</param>
+    /// <returns>
+    /// The transformed word with the first letter in uppercase and the remaining letters in lowercase.
+    /// If the input string is null or empty, the original string is returned.
+    /// </returns>
     static string ToUpper(string word)
     {
-        if (string.IsNullOrEmpty(word)) return word;
+        if (string.IsNullOrEmpty(word)) 
+            return word;
+
         return char.ToUpper(word[0]) + word.Substring(1).ToLower();
     }
 }

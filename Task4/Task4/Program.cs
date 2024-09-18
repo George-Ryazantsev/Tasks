96bee7e563be6385a0c536228ea0404d2417c7fd
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2024-09-18 14:23:09 +0300

    Some commit

--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -1,20 +1,29 @@
 ﻿class Program
 {
+    /// <summary>
+    /// Main method that reads a text and a search word from the user, then calculates how many times the search word appears in the text, ignoring case.
+    /// </summary>
     static void Main()
     {
         int wordCount = 0;
+
         Console.WriteLine("Введите текст:");
-        string inputText = Console.ReadLine();        
+        string inputText = Console.ReadLine();  
+        
         Console.WriteLine("Введите слово для поиска:");
-        string searchWord = Console.ReadLine();        
+        string searchWord = Console.ReadLine();   
+        
         string lowText = inputText.ToLower();
-        string lowWord = searchWord.ToLower();                
-        int index = lowText.IndexOf(lowWord);        
+        string lowWord = searchWord.ToLower();   
+        
+        int index = lowText.IndexOf(lowWord);    
+        
         while (index != -1)
         {
             wordCount++;
             index = lowText.IndexOf(lowWord, index + lowWord.Length);
-        }        
+        }    
+        
         Console.WriteLine($"Слово \"{searchWord}\" встречается в тексте {wordCount} раз(а)");
     }
 }

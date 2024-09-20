184f95ec7d759536b1e406f5e608dce5b63e71b6
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2024-09-20 15:06:55 +0300

    Some commit

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -1,24 +1,20 @@
-﻿class Program
+﻿using System.Text.RegularExpressions;
+
+class Program
 {
     static void Main()
-    {        
+    {
         Console.WriteLine("Введите предложение:");
         string input = Console.ReadLine();
-        
-        string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        
-        int wordCount = words.Length;
-        Console.WriteLine($"Количество слов в предложении: {wordCount}");
-        
-        string[] capitalizedWords = words.Select(word => ToUpper(word)).ToArray();
-        
-        Array.Sort(capitalizedWords);
-        
-        Console.WriteLine("Слова в отсортированном виде:");
 
+        string[] capitalizedWords = RegexActions(ref input).Select(word => ToUpper(word)).ToArray();
+
+        Array.Sort(capitalizedWords, StringComparer.CurrentCultureIgnoreCase);
+
+        Console.WriteLine("Слова в отсортированном виде:");
         foreach (string word in capitalizedWords)
         {
-            Console.WriteLine(word);
+            Console.WriteLine(" " + word);
         }
     }
 
@@ -26,15 +22,29 @@
     /// Converts the first letter of the given word to uppercase, and the rest to lowercase.
     /// </summary>
     /// <param name="word">The word to be transformed.</param>
-    /// <returns>
-    /// The transformed word with the first letter in uppercase and the remaining letters in lowercase.
-    /// If the input string is null or empty, the original string is returned.
-    /// </returns>
+    /// <returns> The transformed word with the first letter in uppercase and the remaining letters in lowercase. </returns>
     static string ToUpper(string word)
     {
-        if (string.IsNullOrEmpty(word)) 
+        if (string.IsNullOrEmpty(word))
             return word;
 
         return char.ToUpper(word[0]) + word.Substring(1).ToLower();
+    }
+
+    /// <summary>
+    /// Extracts words and numbers from the input string using a regular expression and returns them as an array, counts the number of words or numeric sequences.    
+    /// </summary>
+    /// <param name="input">The input string from which words and numbers will be extracted. Passed by reference.</param>
+    /// <returns> An array of strings containing only the words and numbers found in the input.</returns>
+    static string[] RegexActions(ref string input)
+    {
+        string pattern = @"\w+";
+        MatchCollection matches = Regex.Matches(input, pattern);
+
+        string[] words = matches.Cast<Match>().Select(m => m.Value).ToArray();
+        int wordCount = words.Length;
+        Console.WriteLine($"Количество слов в предложении: {wordCount}");
+
+        return words;
     }
 }

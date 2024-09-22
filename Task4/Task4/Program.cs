fc8be41a4c0cc6a15340b3cbd949631528b0c5cd
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2024-09-22 13:54:15 +0300

    Some commit

--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -3,27 +3,37 @@
     /// <summary>
     /// Main method that reads a text and a search word from the user, then calculates how many times the search word appears in the text, ignoring case.
     /// </summary>
-    static void Main()
+    static async Task Main()
     {
-        int wordCount = 0;
-
         Console.WriteLine("Введите текст:");
-        string inputText = Console.ReadLine();  
-        
+        string input = Console.ReadLine();
+
         Console.WriteLine("Введите слово для поиска:");
-        string searchWord = Console.ReadLine();   
-        
+        string searchWord = Console.ReadLine();
+
+        int wordCount = await FindWordAsync(input, searchWord);
+        Console.WriteLine($"Слово \"{searchWord}\" встречается в тексте {wordCount} раз(а)");
+    }
+
+    /// <summary>
+    /// Asynchronous method that counts the occurrences of a search word in the text, ignoring case.
+    /// </summary>    
+    /// <param name="searchWord">Search word</param>
+    /// <returns>The number of occurrences of the search word</returns>
+    static async Task<int> FindWordAsync(string inputText, string searchWord)
+    {
         string lowText = inputText.ToLower();
-        string lowWord = searchWord.ToLower();   
-        
-        int index = lowText.IndexOf(lowWord);    
-        
+        string lowWord = searchWord.ToLower();
+
+        int wordCount = 0;
+        int index = lowText.IndexOf(lowWord);
+
         while (index != -1)
         {
             wordCount++;
             index = lowText.IndexOf(lowWord, index + lowWord.Length);
-        }    
-        
-        Console.WriteLine($"Слово \"{searchWord}\" встречается в тексте {wordCount} раз(а)");
+        }
+
+        return wordCount;
     }
 }

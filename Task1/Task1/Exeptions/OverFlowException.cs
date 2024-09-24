60f820fac22c2f2bd0e59acfffbb73c89c5a09e7
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2024-09-25 01:56:22 +0300

    Some commit

---
+++ Task1/Task1/Exeptions/OverFlowException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Exeptions
+{
+    internal class OverFlowException : Exception
+    {
+        public OverFlowException()
+        {
+            Console.WriteLine(" Error: число превышает допустимый диапазон значений!");
+            Environment.Exit(1);
+        }
+    }
+}
